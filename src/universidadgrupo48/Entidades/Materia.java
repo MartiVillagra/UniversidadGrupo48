7c7b73a674a9e3ba8bddc28abe299d096add072e
purplefox1977 <141675561+purplefox1977@users.noreply.github.com>
2023-09-07 16:50:56 -0300

    0

--- src/universidadgrupo48/Entidades/Materia.java
+++ src/universidadgrupo48/Entidades/Materia.java
@@ -10,5 +10,56 @@ package universidadgrupo48.Entidades;
  * @author Nova
  */
 public class Materia {
-    
+     private int idMatreia;
+    private String nombre;
+    private int anioMateria;
+    private boolean activo;
+
+    public Materia() {
+    }
+
+    public Materia(String nombre, int anioMateria, boolean activo) {
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public Materia(int idMatreia, String nombre, int anioMateria, boolean activo) {
+        this.idMatreia = idMatreia;
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public int getIdMatreia() {
+        return idMatreia;
+    }
+
+    public void setIdMatreia(int idMatreia) {
+        this.idMatreia = idMatreia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioMateria() {
+        return anioMateria;
+    }
+
+    public void setAnioMateria(int anioMateria) {
+        this.anioMateria = anioMateria;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
