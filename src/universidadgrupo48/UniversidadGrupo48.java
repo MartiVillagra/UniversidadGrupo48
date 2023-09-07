863a87cb446d5b0a73ee3174864aacebecabcfff
MartiVillagra <bartivillagra@gmail.com>
2023-09-07 16:57:22 -0300

    acceso a datos

--- src/universidadgrupo48/UniversidadGrupo48.java
+++ src/universidadgrupo48/UniversidadGrupo48.java
@@ -15,7 +15,7 @@ public class UniversidadGrupo48 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Conexion con = new Conexion("universidadgr48");
+        Conection con = new Conection("universidadgr48");
     }
     
 }
