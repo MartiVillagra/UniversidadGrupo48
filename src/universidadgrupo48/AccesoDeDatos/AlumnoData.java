15663fcf6a36c8cc541f7f69d87081d1facd7ef7
MartiVillagra <bartivillagra@gmail.com>
2023-09-08 17:26:23 -0300

    Cargar Alumno

--- src/universidadgrupo48/AccesoDeDatos/AlumnoData.java
+++ src/universidadgrupo48/AccesoDeDatos/AlumnoData.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package universidadgrupo48.AccesoDeDatos;
 
-/**
- *
- * @author Loboplateado77
- */
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import universidadgrupo48.Entidades.Alumno;
+
 public class AlumnoData {
+    private Connection con=null;
     
+    public AlumnoData (){
+     con=Conexion.conectar();   
+    }
+    
+    public void guardarAlumno(Alumno alumno){
+        String sql="INSERT INTO alumno (dni, apellido, nombre, fechaNacimiento, estado)"
+                + "VALUES (?,?,?,?,?)";
+        try {
+            
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
+            ps.setBoolean(5, alumno.isEstado());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                alumno.setIdAlumno(rs.getInt(1));
+             JOptionPane.showMessageDialog(null, "Alumno cargado");   
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR ALUMNO");
+        }
+        
+    }
 }
--- src/universidadgrupo48/UniversidadGrupo48.java
+++ src/universidadgrupo48/UniversidadGrupo48.java
@@ -2,12 +2,20 @@
 package universidadgrupo48;
 
 import java.sql.*;
-import universidadgrupo48.AccesoDeDatos.Conexion;
+import java.time.LocalDate;
+import java.time.Month;
+import universidadgrupo48.AccesoDeDatos.AlumnoData;
+import universidadgrupo48.Entidades.Alumno;
 
 public class UniversidadGrupo48 {
 
     public static void main(String[] args) {
-        Connection con = Conexion.conectar();
+        Alumno juan = new Alumno(123986543,"Ma","Jbh",LocalDate.of(2021, Month.MARCH, 3),true);
+        AlumnoData alu = new AlumnoData();
+        alu.guardarAlumno(juan);
+        
+        
+        
     }
     
 }
