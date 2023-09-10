3c72a8ad8d5535da676debd113204682b7b91de4
purplefox1977 <141675561+purplefox1977@users.noreply.github.com>
2023-09-10 13:32:33 -0300

    Carga alumnos

--- src/universidadgrupo48/UniversidadGrupo48.java
+++ src/universidadgrupo48/UniversidadGrupo48.java
@@ -13,9 +13,47 @@ public class UniversidadGrupo48 {
         Alumno juan = new Alumno(123986543,"Ma","Jbh",LocalDate.of(2021, Month.MARCH, 3),true);
         AlumnoData alu = new AlumnoData();
         alu.guardarAlumno(juan);
-        
-        
-        
+        // Caga de alumnos
+        Alumno nuevoAlu = new Alumno(28565768,"Moreno","Mariano",LocalDate.of(1978, Month.MARCH, 24),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+        //2 
+        nuevoAlu = new Alumno(43567345,"Martinez","Carlos",LocalDate.of(1999, Month.APRIL, 2),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+        //3
+        nuevoAlu = new Alumno(42345888,"Kirchuk","Ernesto",LocalDate.of(2000, Month.AUGUST, 15),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+        //4
+        nuevoAlu = new Alumno(40678435,"Groppa","Mariano",LocalDate.of(1998, Month.SEPTEMBER, 18),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+        //5
+        nuevoAlu = new Alumno(39123987,"Groisman","Bernardo",LocalDate.of(1997, Month.SEPTEMBER, 28),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+        //6
+        nuevoAlu = new Alumno(29875676,"Alonso","Pablo",LocalDate.of(1979, Month.DECEMBER, 12),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+        //7
+        nuevoAlu = new Alumno(30789789,"Pernia","Eric",LocalDate.of(1980, Month.JULY, 17),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+        //8
+        nuevoAlu = new Alumno(30454898,"Montero ","Maria",LocalDate.of(1980, Month.MAY, 30),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+        //9
+        nuevoAlu = new Alumno(29708123,"Alonso","Nicolas",LocalDate.of(1979, Month.FEBRUARY, 25),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+        //10
+        nuevoAlu = new Alumno(28778456,"Pillon","Ignacio",LocalDate.of(1978, Month.JUNE ,13),true);
+        alu = new AlumnoData();
+        alu.guardarAlumno(nuevoAlu);
+
     }
     
 }
